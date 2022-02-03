5403aa1f53d29db704259fb26ac4bc3751e1bdd9
Steve <johnw@motif.foundation>
2022-02-03 14:00:00 +0300

    Update itemExchange.ts

--- src/itemExchange.ts
+++ src/itemExchange.ts
@@ -333,10 +333,10 @@ export function handleBidFinalized(event: BidFinalized): void {
     log.error('Item is null for tokenId: {}', [tokenId])
   }
 
-  let bid = Bid.load(bidId)
-  if (bid == null) {
-    log.error('Bid is null for bidId: {}', [bidId])
-  }
+  // let bid = Bid.load(bidId)
+  // if (bid == null) {
+  //   log.error('Bid is null for bidId: {}', [bidId])
+  // }
 
   let inactiveBidId = tokenId
     .concat('-')
@@ -376,14 +376,18 @@ export function handleBidFinalized(event: BidFinalized): void {
     onChainBid.sellOnShare.value,
     bidder,
     recipient,
-    bid.createdAtTimestamp,
-    bid.createdAtBlockNumber,
+    onChainBid.createdAtTimestamp,
+    onChainBid.createdAtBlockNumber,
     event.block.timestamp,
     event.block.number
   )
 
   // Update Currency Liquidity
-  currency.liquidity = currency.liquidity.minus(bid.amount)
+  //currency.liquidity = currency.liquidity.minus(bid.amount)
+  currency.liquidity = currency.liquidity.minus(onChainBid.amount)
+
+
+  
   currency.save()
 
   // Remove Bid
